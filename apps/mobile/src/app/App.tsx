39c008d453b9ad4dc80a256269ce498fef61b543
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-01-09 10:23:03 -0500

    Update README, ignore all warnings

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -4,6 +4,7 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import { NavigationContainer } from '@react-navigation/native';
+import { LogBox } from 'react-native';
 
 import store from './store';
 
@@ -11,6 +12,7 @@ import RootStack from '../navigators/RootStack';
 
 export default function App() {
   const persistor = persistStore(store);
+  LogBox.ignoreAllLogs();
 
   return (
     <ReduxProvider store={store}>
